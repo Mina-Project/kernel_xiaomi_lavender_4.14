4d663b733388155f3efffdd2e9a9ca8296c5fab6
Danny Lin <danny@kdrag0n.dev>
2019-04-10 22:53:46 -0700

    cpufreq: Always report schedutil governor to libperfmgr
    libperfmgr reads scaling_governor and bails out if the governor is not
    set to schedutil. Bypass this restriction by always reporting schedutil
    when libperfmgr checks this attribute.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,6 +33,7 @@
 #include <linux/sched/topology.h>
 #include <linux/sched/sysctl.h>
 
+#include <linux/binfmts.h>
 #include <trace/events/power.h>
 
 static LIST_HEAD(cpufreq_policy_list);
@@ -767,7 +768,9 @@ static ssize_t show_cpuinfo_cur_freq(struct cpufreq_policy *policy,
  */
 static ssize_t show_scaling_governor(struct cpufreq_policy *policy, char *buf)
 {
-	if (policy->policy == CPUFREQ_POLICY_POWERSAVE)
+	if (task_is_booster(current))
+		return sprintf(buf, "schedutil\n");
+	else if (policy->policy == CPUFREQ_POLICY_POWERSAVE)
 		return sprintf(buf, "powersave\n");
 	else if (policy->policy == CPUFREQ_POLICY_PERFORMANCE)
 		return sprintf(buf, "performance\n");
